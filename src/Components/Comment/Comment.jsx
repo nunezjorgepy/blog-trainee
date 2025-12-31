3a052057209506b93970c0d911bd35a990072602
Jorge Nuñez <nunez.jorge@hotmail.com>
2025-12-31 17:49:42 -0300

    Crea el CommentComponente, distinguindo entre comentario y respuesta

---
+++ src/Components/Comment/Comment.jsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router'
+import './Comment.css'
+
+function Comment(props) {
+    const { isAnswer } = props
+    return (
+        <div className={`comment_container + ${isAnswer && 'answer'}`}>
+            {/* Información del comentario */}
+            <div className="comment_info">
+                <Link to={'/user/jorgenunez'} className="comment_by">
+                    jorgenunez
+                </Link>
+                <div className="comment_date">
+                    28/11/2025 22:15
+                </div>
+            </div>
+            {/* Comentario */}
+            <div className="comment_text">
+                <p className="comment_paragraph">
+                    Este es el comentario que hago sobre un artículo
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Comment
--- src/pages/Article/ArticlePage.jsx
+++ src/pages/Article/ArticlePage.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router"
 import HeaderComponent from "../../Components/Header/HEaderComponent"
 import Article from '../../Components/Article/Article'
 import ShareSocial from '../../Components/ShareSocial/ShareSocial'
+import Comment from '../../Components/Comment/Comment'
 
 /* 
 TODO:
@@ -31,40 +32,9 @@ function ArticlePage() {
                         {/* Grupo de comentarios y respuestas */}
                         <div className="comment_group">
                             {/* Comentario */}
-                            <div className="comment_container">
-                                {/* Información del comentario */}
-                                <div className="comment_info">
-                                    <Link to={'/user/jorgenunez'} className="comment_by">
-                                        jorgenunez
-                                    </Link>
-                                    <div className="comment_date">
-                                        28/11/2025 22:15
-                                    </div>
-                                </div>
-                                {/* Comentario */}
-                                <div className="comment_text">
-                                    <p className="comment_paragraph">
-                                        Este es el comentario que hago sobre un artículo
-                                    </p>
-                                </div>
-                            </div>
+                            <Comment />
                             {/* Respuesta */}
-                            <div className="comment_container answer">
-                                <div className="comment_info">
-                                    <Link to={'/user/tamaranunez'} className="comment_by">
-                                        tamaranunez
-                                    </Link>
-                                    <div className="comment_date">
-                                        28/11/2025 22:20
-                                    </div>
-                                </div>
-
-                                <div className="comment_text">
-                                    <p className="comment_paragraph">
-                                        Y esta es la primer respuesta al comentaario
-                                    </p>
-                                </div>
-                            </div>
+                            <Comment isAnswer />
                         </div>
                     </div>
                 </section>
