d0215df42a577a9f66ecbe0b89dc005f4525626e
Jorge Nuñez <nunez.jorge@hotmail.com>
2026-01-01 17:03:28 -0300

    Crea la página para crear artículos, con sus estilos

--- src/pages/CreateArticle/CreateArticlePage.jsx
+++ src/pages/CreateArticle/CreateArticlePage.jsx
@@ -1,3 +1,4 @@
+import './CreateArticlePage.css'
 import HeaderComponent from "../../Components/Header/HEaderComponent"
 
 
@@ -5,8 +6,57 @@ function CreateArticlePage() {
     return (
         <>
             <HeaderComponent />
-            <main>
-                Crear Artículo
+            <main className="creat_article_main">
+                <section>
+                    <div className="create_form_flex">
+                        <h2 className="create_article_title title">
+                            Crea un nuevo artículo
+                        </h2>
+                        <div className="create_article_container">
+                            <form className="create_article_form">
+                                {/* Título */}
+                                <div className="create_article_titulo form_group">
+                                    <label htmlFor="create_title" className="create_article_title_label required">
+                                        Título
+                                    </label>
+                                    <input 
+                                        type="text" 
+                                        className="create_article_title_input input_with_icon create_width" 
+                                        id="create_title" 
+                                        placeholder='Escribí el título'/>
+                                </div>
+                                <div className="create_article_textarea_container">
+                                    <label htmlFor="create_paragraphs" className="create_article_textarea required">
+                                        Artículo
+                                    </label>
+                                    <textarea 
+                                        type="text" 
+                                        className="create_paragraphs input_with_icon create_width" 
+                                        id="create_paragraphs"
+                                        placeholder='Escribí el artículo' 
+                                    />
+                                </div>
+                                <div className="create_article_tags">
+                                    <label htmlFor="create_tags" className="create_article_tags_container required">
+                                        Tags
+                                    </label>
+                                    <input 
+                                        type="text" 
+                                        className="create_article_tags input_with_icon create_width" 
+                                        id="create_tags" 
+                                        placeholder='Escribí los tags, separados por ;'
+                                    />
+                                </div>
+
+                                <div className="create_article_button_container">
+                                    <button className="create_article_send_data btn-primary">
+                                        Crear
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </section>
             </main>
         </>
     )
